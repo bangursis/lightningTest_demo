d44cd3a9cd425fc3fc37a5ebe83dd0031660bd37
beingofhabits <beingofhabits@gmail.com>
2018-11-02 17:11:36 +0300

    final simple test

--- force-app/main/default/staticresources/testFile.js
+++ force-app/main/default/staticresources/testFile.js
@@ -4,9 +4,6 @@ describe("true-true demo assert", () => {
         $T.createComponent(`c:mainComponent`, {}, true)
             .then(cmp => {
                 expect(cmp.get("v.int")).toBe(0);
-            })
-            .then(cmp => {
-                expect(cmp.get(`v.int`) + 1).toBe(1);
                 done();
             })
         });
